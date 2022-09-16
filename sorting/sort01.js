d69c989f888891d7a989b8f77e253c382385baf2
sunub <bsc5672@gmail.com>
2022-09-17 01:28:17 +0900

    adding how it works

--- sorting/sort01.js
+++ sorting/sort01.js
@@ -1,5 +1,11 @@
 var arr = [4, 2, 1, 3];
 
+/**
+ * This fuction must have to return an array that represents the smallest difference between the two consecutive numbers in the arr.
+ * @param {number} arr
+ * @returns {number[]}
+ */
+
 function minimumAbsDifference(arr) {
   arr.sort(function (a, b) {
     return a - b;
