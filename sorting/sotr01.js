575f81bf36865950865671e1a7f337c263bad8d1
sunub <bsc5672@gmail.com>
2022-09-17 01:12:27 +0900

    sorting algorithm
    This folder is for sorting algorithm

---
+++ sorting/sotr01.js
@@ -0,0 +1,28 @@
+var arr = [4, 2, 1, 3];
+
+function minimumAbsDifference(arr) {
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  var result = [];
+  var instance = new Map();
+  for (var i = 0; i < arr.length - 1; i++) {
+    var diff = Math.abs(arr[i] - arr[i + 1]);
+    instance.set([arr[i], arr[i + 1]], diff);
+  }
+  var min;
+  instance.forEach(function (value) {
+    if (min === undefined) {
+      min = value;
+    }
+    if (min > value) {
+      min = value;
+    }
+  });
+  instance.forEach(function (value, key) {
+    if (value <= min) {
+      result.push(key);
+    }
+  });
+  return result;
+}
